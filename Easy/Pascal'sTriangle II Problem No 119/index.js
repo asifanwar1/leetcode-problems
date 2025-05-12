4c67157a32da7ad200f63892197dabdaba308bd2
asifanwar1 <asifanwar0705@gmail.com>
2025-05-12 11:57:46 +0500

    Problem No 119 Completed

--- Easy/Pascal'sTriangle II Problem No 119/index.js
+++ Easy/Pascal'sTriangle II Problem No 119/index.js
@@ -1,15 +1,16 @@
-const generate = (numRows) => {
-	if (numRows === 1) return [[1]];
+const getRow = (rowIndex) => {
+	if (rowIndex === 0) return [1];
 	
 	let numArr = [[1],];
 
 
 
-	for(let i = 1; i < numRows; i++){
+	for(let i = 1; i < rowIndex + 1; i++){
 		
 		let newRow = [1,1];
-		let x = numArr[i-1]
 		
+		let x = numArr[0]
+
 
 		for(let j = 0; j < x.length; j++){
 			let l = x[j] + x[j+1]
@@ -19,17 +20,42 @@ const generate = (numRows) => {
 				break;
 			}
 
+			
 			newRow.splice(1, 0, l)
 		}
-		numArr.push(newRow)
-
+		numArr.shift();
+		numArr = [...numArr, newRow];
+		
+	
 	}
 
-	return numArr;
+	return numArr.flat();
 }
 
 
-let num = 5;
-console.log(generate(num));
+let num = 3;
+console.log(getRow(num));
+
+
+
+//By Chtagpt
+// This function generates the kth row of Pascal's triangle
+// using an iterative approach. It starts with the first row and builds
+
+const getRow2 = (rowIndex) => {
+    const row = [1]; // Start with the first element
+
+    // Build the row one element at a time
+    for (let i = 1; i <= rowIndex; i++) {
+        // Update from right to left to avoid overwriting values needed later
+        for (let j = row.length - 1; j >= 1; j--) {
+            row[j] = row[j] + row[j - 1];
+        }
+        row.push(1); // Every row ends with a 1
+    }
+
+    return row;
+}
+
 
 
