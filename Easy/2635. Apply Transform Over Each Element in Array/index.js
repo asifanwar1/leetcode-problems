4f059a77d3d86f9071ac5ad45625a8d5af4e78f6
asifanwar1 <asifanwar0705@gmail.com>
2025-05-15 14:39:24 +0500

    problem 2635 solved

---
+++ Easy/2635. Apply Transform Over Each Element in Array/index.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+
+    return result;
+};
+
+// let arr = [1, 2, 3];
+// let fn = function plusone(n) {
+//     return n + 1;
+// };
+
+// let arr = [1, 2, 3];
+// let fn = function plusI(n, i) {
+//     return n + i;
+// };
+
+let arr = [10, 20, 30];
+let fn = function constant() {
+    return 42;
+};
+
+console.log(map(arr, fn));
